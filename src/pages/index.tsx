e611b47c4019e25cfb3f7b7cf3c3c05db10f4ca4
Firdous Ahmad Wani <149361552+firdous-wani@users.noreply.github.com>
2025-03-28 11:33:01 +0530

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -253,7 +253,7 @@ const Home: NextPage = () => {
             </div>
             <div className="mt-8 md:order-1 md:mt-0">
               <p className="text-center text-xs leading-5 text-gray-500">
-                &copy; 2024 ProjectMgr. All rights reserved.
+                ProjectMgr. All rights reserved.
               </p>
             </div>
           </div>
